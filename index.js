c13442c5c83b60747d00fbbd3e8f75937cc87485
Mendy <mendyw94@gmail.com>
2022-04-27 20:13:34 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+
+function submitData (userName, userEmail) {
+    const bodyObj = {
+        name: `${userName}`,
+        email: `${userEmail}`
+    }
+    const configurationObj = {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body:JSON.stringify(bodyObj)
+    }
+    return fetch("http://localhost:3000/users", configurationObj) 
+            .then((resp) => {return resp.json()})
+            .then((data) => {
+                // const newDiv = document.createElement('div')
+                const newParagraph = document.createElement('p')
+
+                newParagraph.textContent = data.id;
+
+                document.body.append(newParagraph);
+                // document.querySelector('p').textContent = 'hi'//data.id
+            })
+            .catch((message) => {
+                const newParagraph = document.createElement('p')
+
+                newParagraph.textContent = message;
+
+                document.body.append(newParagraph);
+            })
+        }
